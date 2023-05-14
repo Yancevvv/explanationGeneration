61f94cbd0a3ec41fbc617dd049af999576daad2c
Yancevvv <al888a@mail.ru>
2023-05-14 23:40:27 +0300

    Added stubs for functions in main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QCoreApplication>
+#include <QtXml/qdom.h>
 
 void preorder(const QDomNode &node, QVector<QString> &nodeInformation);
 void collectData(const QDomElement &domElement, QVector<QString> &nodeInformation);
@@ -10,4 +11,20 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     return a.exec();
+}
+
+void preorder(const QDomNode &node, QVector<QString> &nodeInformation) {
+
+}
+
+void collectData(const QDomElement &domElement, QVector<QString> &nodeInformation) {
+
+}
+
+void formSentence(QVector<QString> vectorStr, QString &expressionInformation) {
+
+}
+
+QString getGivenWordForm(QString setGivenWords, QString Case) {
+    return "";
 }
